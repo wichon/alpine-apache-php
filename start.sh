08ec286a911071febadb582fbf276b4024ff221d
Luis Alonzo <wichon@gmail.com>
2016-06-16 11:16:17 -0600

    Update start.sh
    Added AllowOverride All for just the web app root

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 
 sed -i "s#^DocumentRoot \".*#DocumentRoot \"/app/$WEBAPP_ROOT\"#g" /etc/apache2/httpd.conf
 sed -i "s#/var/www/localhost/htdocs#/app/$WEBAPP_ROOT#" /etc/apache2/httpd.conf
+printf "\n<Directory \"/app/$WEBAPP_ROOT\">\n\tAllowOverride All\n</Directory>\n" >> /etc/apache2/httpd.conf
 chown -R apache:apache /app
 
 httpd -D FOREGROUND
